80987506dc946f2cb730c8a6bb05084e2e5fd72d
neitonjc <78852153+neitonjc@users.noreply.github.com>
2021-10-11 13:51:37 -0300

    implemented subscriptions

--- graphql-spring/src/main/java/com/neitonjc/graphql/resolver/PessoaResolver.java
+++ graphql-spring/src/main/java/com/neitonjc/graphql/resolver/PessoaResolver.java
@@ -1,21 +1,29 @@
 package com.neitonjc.graphql.resolver;
 
 import java.util.Collection;
+import java.util.concurrent.ConcurrentHashMap;
 
+import org.reactivestreams.Publisher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.coxautodev.graphql.tools.GraphQLMutationResolver;
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import com.coxautodev.graphql.tools.GraphQLSubscriptionResolver;
 import com.neitonjc.graphql.input.PessoaInput;
 import com.neitonjc.graphql.model.Pessoa;
 import com.neitonjc.graphql.repository.PessoaRepository;
 
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.FluxSink;
+
 @Component
-public class PessoaResolver implements GraphQLQueryResolver, GraphQLMutationResolver {
+public class PessoaResolver implements GraphQLQueryResolver, GraphQLMutationResolver, GraphQLSubscriptionResolver {
 	
 	@Autowired
 	private PessoaRepository rep;
+	
+	private ConcurrentHashMap<Integer, FluxSink<Pessoa>> peopleSubscribers = new ConcurrentHashMap<>();
 	
 	public Collection<Pessoa> findAllPeople(){
 		return rep.findAll();
@@ -33,6 +41,17 @@ public class PessoaResolver implements GraphQLQueryResolver, GraphQLMutationReso
 		Pessoa p = rep.findById(peopleId).get();
 		p.setEmail(email);
 		
-		return rep.save(p);
+		rep.save(p);
+		
+		if(peopleSubscribers.containsKey(peopleId))
+			peopleSubscribers.get(peopleId).next(p);
+		
+		return p;
+	}
+	
+	public Publisher<Pessoa> onPeopleUpdated(Integer peopleId){
+		return Flux.create(subscriber -> 
+								peopleSubscribers.put(peopleId, subscriber.onDispose(() -> peopleSubscribers.remove(peopleId, subscriber))),
+								FluxSink.OverflowStrategy.LATEST);
 	}
 } 
